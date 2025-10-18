d7b923e88866cc21b9859e1af99ea49e8ba9514e
yeahhabebi <yeahhabebi@gmail.com>
2025-10-18 09:28:59 +0600

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Start backend
-uvicorn backend.app:app --host 0.0.0.0 --port 8000 &
-
-# Start Telegram listener
-python tg_listener/tg_listener.py &
-
-# Start Streamlit dashboard
-streamlit run frontend/dashboard.py --server.port 8501 --server.headless true
